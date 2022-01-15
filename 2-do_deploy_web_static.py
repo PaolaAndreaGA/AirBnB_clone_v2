a1d092facf13982964eb0390d5c3583523c44c95
PaolaAndreaGA <paga0209@gmial.com>
2022-01-14 21:04:05 -0500

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,49 @@
 #!/usr/bin/python3
 # distributes an archive to your web servers, using the function do_deploy:
 
-from fabric.api import *
-import os.path
+from fabric.api import run, put, env, local
+import os
+from datetime import datetime
+
 
 env.user = 'ubuntu'
-env.hosts = ['34.138.47.31', '54.226.1.15']
+env.hosts = ['54.226.1.15']
+
+
+def do_pack():
+    """
+    Function to create a .tgz archive.
+    """
+    try:
+        local("mkdir -p versions")
+        d_t = datetime.now().strftime("%Y%m%d%H%M%S")
+        compr_file = "versions/web_static_{}.tgz".format(d_t)
+        local("tar -cvzf {} web_static".format(compr_file))
+        return compr_file
+    except exception:
+        return None
 
 
 def do_deploy(archive_path):
-    """Fabric script that distrubutes an archive to server"""
+    """
+    Function that distributes an archive.
+    """
+    name_dir = archive_path[9:-4]
     if os.path.exists(archive_path):
-        new_path = archive_path.replace('versions/', '')
-        file_name = new_path[:-4]
-        arc_folder = "/data/web_static/releases/{}".format(new_path)
         put(archive_path, '/tmp/')
-        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
-        run('tar -xzf /tmp/{} -C {}'.format(new_path, arc_folder[:-4]))
-        run('rm /tmp/{}'.format(new_path))
-        run('mv {}/web_static/* {}/'.format(arc_folder[:-4], arc_folder[:-4]))
-        run('rm -rf {}/web_static'.format(arc_folder[:-4]))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(arc_folder[:-4]))
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(name_dir))
+        run("sudo tar -xzf /tmp/{}.tgz -C \
+            /data/web_static/releases/{}/".format(name_dir, name_dir))
+        run("sudo rm /tmp/{}.tgz".format(name_dir))
+        run("sudo mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".
+            format(name_dir, name_dir))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static"
+            .format(name_dir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current".format(name_dir))
+        print("New version deployed!")
         return True
     else:
         return False
