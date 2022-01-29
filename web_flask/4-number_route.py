86e26ab4b84153924cd087b7048a44892a122c4c
PaolaAndreaGA <paga0209@gmial.com>
2022-01-29 17:49:58 -0500

    4-number_route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -35,7 +35,7 @@ def pcool(text="is cool"):
     return "Python {}".format(ptext)
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def n_int(n):
     """display “n is a number” only if n is an integer """
     if type(n) is int:
