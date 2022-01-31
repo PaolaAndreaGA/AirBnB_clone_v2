1c72d5b07be0add91199a8786d9f94da1a629191
Paola Andrea García Altamirano <85592476+PaolaAndreaGA@users.noreply.github.com>
2022-01-30 21:13:48 -0500

    Update 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  starts a Flask web application """
+""" Flask web application """
 
 from http.client import FOUND
 from flask import Flask, render_template
@@ -8,27 +8,27 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def storage_close(self):
-    """ Data from source """
-    storage.close()
-
-
 @app.route('/states', strict_slashes=False)
 def states():
-    """ /states path """
+    """ states path """
     states = storage.all(State)
     return render_template('9-states.html', states=states, id=False)
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
-    """ /states/<id> path """
+    """ states_id_path """
     states = storage.all(State)
     for state in states.values():
         if state.id == str(id):
             return render_template('9-states.html', states=state)
         return render_template('9-states.html', not_found=True)
+    
+    
+@app.teardown_appcontext
+def storage_close(self):
+    """ Datasource """
+    storage.close()
 
 
 if __name__ == '__main__':
